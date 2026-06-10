ac2bd807ba0585000342865eb264cef03de338ae
benw88-creator <gd8wkw9vnf@privaterelay.appleid.com>
2026-06-10 11:33:20 +0100

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,12 +1,30 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const code = req.query.code;
 
   if (!code) {
-    return res.status(400).send("No code from Spotify");
+    return res.status(400).send("No code received");
   }
 
-  res.status(200).json({
-    message: "Callback works",
-    code: code
+  const client_id = process.env.SPOTIFY_CLIENT_ID;
+  const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+  const redirect_uri = process.env.REDIRECT_URI;
+
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization:
+        "Basic " +
+        Buffer.from(client_id + ":" + client_secret).toString("base64"),
+    },
+    body: new URLSearchParams({
+      grant_type: "authorization_code",
+      code,
+      redirect_uri,
+    }),
   });
+
+  const data = await response.json();
+
+  res.status(200).json(data);
 }
