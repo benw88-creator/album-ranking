d168ac7dc53220aad30bc9e4fe0f8b8ad02a5323
benw88-creator <gd8wkw9vnf@privaterelay.appleid.com>
2026-06-10 11:24:25 +0100

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,6 +1,17 @@
 export default function handler(req, res) {
-  res.status(200).json({
-    client_id: process.env.SPOTIFY_CLIENT_ID || "MISSING",
-    redirect_uri: process.env.REDIRECT_URI || "MISSING"
+  const client_id = process.env.SPOTIFY_CLIENT_ID;
+  const redirect_uri = process.env.REDIRECT_URI;
+
+  const scope = "user-read-email";
+
+  const params = new URLSearchParams({
+    response_type: "code",
+    client_id,
+    scope,
+    redirect_uri,
   });
+
+  const url = "https://accounts.spotify.com/authorize?" + params.toString();
+
+  res.redirect(url);
 }
