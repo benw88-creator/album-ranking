cee4ac6630504e444258bb78e11b027c8ecd31dd
benw88-creator <gd8wkw9vnf@privaterelay.appleid.com>
2026-06-10 11:19:59 +0100

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,1 +1,6 @@
-
+export default function handler(req, res) {
+  res.status(200).json({
+    client_id: process.env.SPOTIFY_CLIENT_ID || "MISSING",
+    redirect_uri: process.env.REDIRECT_URI || "MISSING"
+  });
+}
